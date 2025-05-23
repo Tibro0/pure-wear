86ada2a8f31d78f6a11e2c4409b556d6511c5fe9
Tibro0 <faysaltibro@gmail.com>
2025-05-24 00:51:03 +0600

    Admin Authentication With Laravel Api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,10 @@ import Shop from './components/Shop'
 import Product from './components/Product'
 import Cart from './components/Cart'
 import Checkout from './components/Checkout'
+import Login from './components/admin/Login'
+import { ToastContainer, toast } from 'react-toastify';
+import Dashboard from './components/admin/Dashboard'
+import { AdminRequireAuth } from './components/admin/AdminRequireAuth'
 
 function App() {
 
@@ -12,13 +16,22 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/shop' element={<Shop/>}/>
-          <Route path='/product' element={<Product/>}/>
-          <Route path='/cart' element={<Cart/>}/>
-          <Route path='/checkout' element={<Checkout/>}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/product' element={<Product />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/checkout' element={<Checkout />} />
+
+          <Route path='/admin/login' element={<Login />} />
+          <Route path='/admin/dashboard' element={
+            <AdminRequireAuth>
+              <Dashboard/>
+            </AdminRequireAuth>
+          } />
         </Routes>
       </BrowserRouter>
+
+      <ToastContainer />
     </>
   )
 }
